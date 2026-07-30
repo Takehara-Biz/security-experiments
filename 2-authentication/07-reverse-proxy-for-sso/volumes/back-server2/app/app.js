93f2c206b5ffa95c7781ec24897d161f553991e6
Yasushi20260708 <takehara@liberty-it.biz>
2026-07-30 14:08:38 +0900

    update

--- 2-authentication/07-reverse-proxy-for-sso/volumes/back-server2/app/app.js
+++ 2-authentication/07-reverse-proxy-for-sso/volumes/back-server2/app/app.js
@@ -3,6 +3,17 @@ const PORT = 3002;
 
 http.createServer((req, res) => {
   const userId = req.headers['x-user-id'];
+  if (!userId){
+    // 本来は、userIdがあるかどうかではなく、長い英数字（JWTなど）を指定させて、その内容を検証するべきだが、今回は簡易版なので省略している。
+    res.writeHead(403, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(`
+      <h1>[ App 2 ] 勤怠管理システム</h1>
+      <p>認証されていないため、サービスをご利用できません</p>
+      <a href="http://localhost:3000/">ログインページ（プロキシサーバ）へ移動</a>
+    `);
+    return;
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.end(`
     <h1>[ App 2 ] 勤怠管理システム</h1>
