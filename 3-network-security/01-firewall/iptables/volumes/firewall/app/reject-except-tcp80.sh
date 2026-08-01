b85096e2103e7e3f6ec61a99e4358efa58b812ce
Yasushi20260708 <takehara@liberty-it.biz>
2026-08-01 12:53:09 +0900

    update

--- 3-network-security/01-firewall/iptables/volumes/firewall/app/reject-except-tcp80.sh
+++ 3-network-security/01-firewall/iptables/volumes/firewall/app/reject-except-tcp80.sh
@@ -3,7 +3,14 @@
 set -euo pipefail
 set -x
 
+# 現在のルールを確認
+iptables -L -n --line-numbers
+# 既存のFORWARDチェインのルールをすべて削除
 iptables -F FORWARD
+# すでに接続済みの通信（ESTABLISHED）と、それに関連して発生した通信（RELATED）の通過を許可します。内部からWebサイトにアクセスした際、その「戻りのパケット」が遮断されないようにします。
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
+# FORWARDチェインのデフォルトポリシーを「破棄（DROP）」に設定します。上記の許可ルールに一致しなかったすべての通信を拒否する安全な状態にします。
 iptables -P FORWARD DROP
+
+iptables -L -n --line-numbers
